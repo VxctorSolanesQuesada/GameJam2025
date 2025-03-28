b5171b18e645c2d20a48d903021290f75f51e234
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2025-03-28 18:25:55 +0100

    sin comentario...

--- Assets/Scripts/CatWrong.cs
+++ Assets/Scripts/CatWrong.cs
@@ -8,20 +8,12 @@ public class CatWrong : MonoBehaviour
 
     private void Start()
     {
-        // Verificar si el componente CatCorrect está en el mismo GameObject
         verify = GetComponent<CatCorrect>();
 
-        // Si no lo encontramos, buscarlo en el GameObject del prefab asociado
         if (verify == null)
         {
-            // Suponiendo que el prefab de gato está en el mismo GameObject, buscarlo de esta manera
             verify = FindObjectOfType<CatCorrect>();
 
-            // Si tampoco lo encontramos, mostrar un mensaje de error
-            if (verify == null)
-            {
-                Debug.LogError("CatCorrect no encontrado en el objeto.");
-            }
         }
     }
 
@@ -32,41 +24,31 @@ public class CatWrong : MonoBehaviour
 
     private IEnumerator HandleWrongCatClick()
     {
-        // Esperar 0.2 segundos antes de proceder
         yield return new WaitForSeconds(0.05f);
 
-        // Verificar si el componente `CatCorrect` no es null y si no ha sido tocado
         if (verify != null && !verify.touched)
         {
-            // Buscar todos los gatos existentes
             GameObject[] existingCats = GameObject.FindGameObjectsWithTag("Cat");
 
             if (existingCats.Length != 0)
             {
                 foreach (GameObject cat in existingCats)
                 {
-                    // Destruir cada gato con un pequeño retraso
                     Destroy(cat);
                 }
             }
 
-            // Encontrar el generador de gatos
             catGen = FindObjectOfType<CatGenerator>();
             catGen.end = true;
 
-            // Mostrar el mensaje de debug
-            Debug.Log("Has hecho clic en el gato incorrecto: " + gameObject.name);
 
-            // Llamar al método GameOver en el GameManager
             GameManager.Instance.GameOver();
 
 
-            // Restablecer el valor de touched
             verify.touched = false;
         }
         else
         {
-            // Mostrar un mensaje de error si `verify` es null
             Debug.LogError("El componente CatCorrect no está asignado correctamente.");
         }
     }
